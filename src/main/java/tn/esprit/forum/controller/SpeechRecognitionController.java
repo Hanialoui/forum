2b690030171e55449ec5e2de00bd95c4d5f6cca5
Hanialoui <hani.aloui@esprit.tn>
2026-04-13 00:19:50 +0100

    heni fixed docker bug

--- src/main/java/tn/esprit/forum/controller/SpeechRecognitionController.java
+++ src/main/java/tn/esprit/forum/controller/SpeechRecognitionController.java
@@ -1,5 +1,6 @@
 package tn.esprit.forum.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
