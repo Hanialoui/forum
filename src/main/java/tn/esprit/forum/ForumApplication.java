86c79ffe6a92e4e5635f1f1190eca16e1932f49a
Hanialoui <hani.aloui@esprit.tn>
2026-05-01 14:35:06 +0200

    Add Redis caching for forum posts

--- src/main/java/tn/esprit/forum/ForumApplication.java
+++ src/main/java/tn/esprit/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package tn.esprit.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableCaching
 @EnableDiscoveryClient
 
 public class ForumApplication {
