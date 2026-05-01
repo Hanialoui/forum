29ed716bf1aceda1bc9b24caeb5cefe2fbfeabf4
Hanialoui <hani.aloui@esprit.tn>
2026-05-01 13:46:35 +0100

    Fix tests with Redis cache disabled

--- src/test/java/tn/esprit/forum/ForumApplicationTests.java
+++ src/test/java/tn/esprit/forum/ForumApplicationTests.java
@@ -1,13 +1,18 @@
 package tn.esprit.forum;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.cache.type=none",
+    "spring.data.redis.host=localhost",
+    "spring.data.redis.port=6379",
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false"
+})
 class ForumApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
